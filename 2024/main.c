84b79c3214f46f24e9bd35c4e389721864053b95
ethanmccullagh <ethan89762@gmail.com>
2024-07-24 13:01:27 -0700

    main.c

---
+++ 2024/main.c
@@ -0,0 +1,12 @@
+#include <stdint.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
+
+int main(int argc, char *argv[]) {
+
+    printf("hello world\n");
+
+    return 0;
+}
