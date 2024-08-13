eabc5ec919b8adcf02e1012cbf5c96bb21e695bb
AmannSingh <amann.singh7@outlook.com>
2024-08-13 16:33:32 -0700

    commented out line 20

--- 2024/optimized.c
+++ 2024/optimized.c
@@ -17,7 +17,7 @@ void normalize();
 
 // Try aligning the matrix data in memory to improve the cache efficiency 
 
-__attribute__(aligned(16))
+//__attribute__(aligned(16))
 
 int32_t matrix[ROWS][COLS * 2] = {
     {7, 7, 8, 3, 2},
